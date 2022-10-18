20ba7683697e318bbdd71005fb8199dde1c747d0
Rinaldo23 <rinaldobadigar98@gmail.com>
2022-10-18 17:33:29 +0530

    RecurssionAndBackTracking

---
+++ RecurssionAndBackTracking/Level_2/PalindromePartitionOfString.java
@@ -0,0 +1,49 @@
+package RecurssionAndBackTracking.Level_2;
+
+import java.util.*;
+
+public class PalindromePartitionOfString {
+
+    public static void solution(String str, String asf) {
+
+        if (str.length() == 0) {
+            System.out.println(asf);
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            String a = str.substring(0, i + 1);
+            String ros = str.substring(i + 1);
+            if (isPalindrome(a)) {
+                solution(ros, asf + "(" + a + ") ");
+            }
+        }
+
+    }
+
+    public static boolean isPalindrome(String str) {
+        int li = 0;
+        int ri = str.length() - 1;
+
+        while (li < ri) {
+            char left = str.charAt(li);
+            char right = str.charAt(ri);
+
+            if (left != right) {
+                return false;
+            }
+
+            li++;
+            ri--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        try (Scanner scn = new Scanner(System.in)) {
+            String str = scn.nextLine();
+            solution(str, "");
+        }
+    }
+
+}
